004798392aea713d2507bb17ae5c9707ac750ef4
Adrien Girardeau <adrigir25@gmail.com>
2016-11-28 16:57:04 +0100

    adding routes to manage users. Those routes are not secure. Security is to be added later.

--- routes/api.js
+++ routes/api.js
@@ -19,10 +19,37 @@ router.get("/api/service/:id", function(req, res, next) {
 
 //User-related routes
 
+//List all users
 router.get("/api/users", function(req, res, next) {
     models.collections.user.find().exec(function(err, result) {
     if(err) return res.json({ err: err }, 500);
     res.json(result);
+  });
+});
+
+//Create a user
+app.post('/api/users', function(req, res) {
+  	models.collections.user.create(req.body, function(err, model) {
+    if(err) return res.json({ err: err }, 500);
+    res.json(model);
+  });
+});
+
+//Delete a user
+app.get('/api/users/:id/delete', function(req, res) {
+  models.collections.user.destroy({ id: req.params.id }, function(err) {
+    if(err) return res.json({ err: err }, 500);
+    res.json({ status: 'ok' });
+  });
+});
+
+//Update a user
+app.put('/api/users/:id/update', function(req, res) {
+  // Don't pass ID to update
+  delete req.body.id;
+  models.collections.user.update({ id: req.params.id }, req.body, function(err, model) {
+    if(err) return res.json({ err: err }, 500);
+    res.json(model);
   });
 });
 
