921caf9e09120f941b8194408aae3ba2c17b92df
Unknown <stratosfr@gmail.com>
2016-12-07 15:01:35 +0100

    Add system info
    Actually can retrieve CPU load and ram usage

---
+++ libs/system.js
@@ -0,0 +1,32 @@
+var fs     = require('fs');
+var os     = require('os');
+var exec = require('child_process').exec;
+var system = [];
+var cpu = os.loadavg();
+
+function freemem(cb) {
+  exec("awk '/MemAvailable/ { print $2 }' /proc/meminfo", function(error, stdout, stderr) {
+  stdout = Number(stdout)*1000;
+  var data = { 'total' : os.totalmem(),
+           'used' : os.totalmem()-stdout,
+           'available' : stdout,
+           'percentage_used' : Math.round(((os.totalmem()-stdout)/os.totalmem())*100),
+           'percentage_available' : Math.round(100-(((os.totalmem()-stdout)/os.totalmem())*100)) };
+  cb(data);
+  })
+};
+
+
+
+system.get = function(cb) {
+  var data = {
+    "CPU" : cpu,
+    "RAM" : "",
+    "DISK" : "to complete",
+    "NET" : "to complete"
+  };
+  freemem(function(res){data.RAM=res; cb(data)});
+}
+
+
+module.exports = system;
