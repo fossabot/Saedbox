c0933dcb36cabb3c65791e5276da3f55acffd7b8
Adrien Girardeau <adrigir25@gmail.com>
2017-01-12 20:57:32 +0100

    Correcting small bugs and adding tests. More tests and debug in the days to come

--- routes/api.js
+++ routes/api.js
@@ -55,7 +55,7 @@ router.post("/api/services", function(req, res, next) {
 //List all users
 router.get("/api/users", function(req, res, next) {
     models.collections.user.find().populate('group','containers').exec(function(err, result) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,result);
   });
 });
@@ -63,7 +63,7 @@ router.get("/api/users", function(req, res, next) {
 //Create a user
 router.post('/api/users', function(req, res) {
     models.collections.user.create(req.body, function(err, model) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,model);
   });
 });
@@ -71,7 +71,7 @@ router.post('/api/users', function(req, res) {
 //Get User infos
 router.get('/api/users/:id', function(req, res) {
     models.collections.user.findOne({ id: req.params.id }, function(err, model) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,model);
   });
 });
@@ -79,7 +79,7 @@ router.get('/api/users/:id', function(req, res) {
 //Delete a user
 router.delete('/api/users/:id', function(req, res) {
   models.collections.user.destroy({ id: req.params.id }, function(err) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,{ status: 'ok' });
   });
 });
@@ -89,7 +89,7 @@ router.put('/api/users/:id', function(req, res) {
   // Don't pass ID to update
   delete req.body.id;
   models.collections.user.update({ id: req.params.id }, req.body, function(err, model) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,model);
   });
 });
@@ -100,7 +100,7 @@ router.put('/api/users/:id', function(req, res) {
 //List all groups
 router.get("/api/groups", function(req, res, next) {
     models.collections.group.find().exec(function(err, result) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,result);
   });
 });
@@ -108,7 +108,7 @@ router.get("/api/groups", function(req, res, next) {
 //Create a group
 router.post('/api/groups', function(req, res) {
     models.collections.group.create(req.body, function(err, model) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,model);
   });
 });
@@ -116,7 +116,7 @@ router.post('/api/groups', function(req, res) {
 //Get group infos
 router.get('/api/groups/:id', function(req, res) {
     models.collections.group.findOne({ id: req.params.id }, function(err, model) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,model);
   });
 });
@@ -124,7 +124,7 @@ router.get('/api/groups/:id', function(req, res) {
 //Delete a group
 router.delete('/api/groups/:id', function(req, res) {
   models.collections.group.destroy({ id: req.params.id }, function(err) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,{ status: 'ok' });
   });
 });
@@ -134,7 +134,7 @@ router.put('/api/groups/:id', function(req, res) {
   // Don't pass ID to update
   delete req.body.id;
   models.collections.group.update({ id: req.params.id }, req.body, function(err, model) {
-    if(err) resp.sendError(res,err);
+    if(err) return resp.sendError(res,err);
     resp.send(res,model);
   });
 });
--- test/User.js
+++ test/User.js
@@ -19,9 +19,9 @@ describe('Users', () => {
 		});        
     });     
 
-	//Testing the GET route
+	//Testing the GET route without data
 	describe('/GET /api/users', () => {
-	    it('it should GET all the users', (done) => {
+	    it('it should GET an empty array', (done) => {
 	        chai.request("http://127.0.0.1:9002")
 	            .get('/api/users')
 	            .end((err, res) => {
@@ -34,9 +34,53 @@ describe('Users', () => {
 	    });
 	});
 
-	//Testing the POST route
+	//Testing the GET route with data
+	describe('/GET /api/users', () => {
+	    it('it should GET all the users', (done) => {
+
+	    	let group = {
+        		name: "Basic",
+        		p_cont_m:true,
+			    o_cont_m:false,
+			    manage_users:false,
+			    manage_groups:false,
+			    p_recp_m:true,
+			    o_recp_m:false,
+			    account_delete:true,
+			    deletable: false
+        	};
+        	models.collections.group.create(group, function(err, model) {
+			    if(err) return(err);
+
+				let users=[
+					{group: model.id, password: "plop", name:"plop1"},
+					{group: model.id, password: "plop", name:"plop2"}
+				];
+
+		    	models.collections.user.create(users, function(err, response) {
+				    if(err) return(err);
+
+
+				    chai.request("http://127.0.0.1:9002")
+			            .get('/api/users')
+			            .end((err, res) => {
+			                res.should.have.status(200);
+			                let data = res.body.data;
+			                data.should.be.a('array');
+			                data.length.should.be.eql(2);
+			            done();
+			        });
+
+				});
+			});
+
+	        
+	    });
+	});
+
+	//Testing the POST route without name
 	describe('/POST /api/users', () => {
-      	it('it should not POST a user without name field', (done) => {
+      	it('it should not POST a user without name fields', (done) => {
         	let group = {
         		name: "Basic",
         		p_cont_m:true,
@@ -55,18 +99,95 @@ describe('Users', () => {
 	            	group: model.id,
 	            	password: "plop"
 	        	};
+
 			   	chai.request("http://127.0.0.1:9002")
 	            	.post('/api/users')
+	            	.set('content-type', 'application/x-www-form-urlencoded')
 	            	.send(user)
-	            	.end((err, res) => {
-	            		let result=res.body;
-	                	res.should.have.status(400);
-	                	result.should.be.a('object');
-	                	result.should.have.property('error');
-	                	result.error.should.have.property('name');
-	                	result.error.name.should.have.property('kind').eql('required');
-	              	done();
-	           	}); 
+					.end(function(error, response) {
+		                let result=error.response.res.body.error;
+		               	result.should.have.status(400);
+		               	result.should.be.a('object');
+		               	result.should.have.property('invalidAttributes');
+		               	result.invalidAttributes.should.have.property('name');
+		              	done();
+		            });
+			});
+      	});
+  	});
+
+	//Testing the POST route without password
+	describe('/POST /api/users', () => {
+      	it('it should not POST a user without password fields', (done) => {
+        	let group = {
+        		name: "Basic",
+        		p_cont_m:true,
+			    o_cont_m:false,
+			    manage_users:false,
+			    manage_groups:false,
+			    p_recp_m:true,
+			    o_recp_m:false,
+			    account_delete:true,
+			    deletable: false
+        	};
+        	models.collections.group.create(group, function(err, model) {
+			    if(err) return(err);
+
+			    let user = {
+	            	group: model.id,
+	            	name: "plop"
+	        	};
+
+			   	chai.request("http://127.0.0.1:9002")
+	            	.post('/api/users')
+	            	.set('content-type', 'application/x-www-form-urlencoded')
+	            	.send(user)
+					.end(function(error, response) {
+		                let result=error.response.res.body.error;
+		               	result.should.have.status(400);
+		               	result.should.be.a('object');
+		               	result.should.have.property('invalidAttributes');
+		               	result.invalidAttributes.should.have.property('password');
+		              	done();
+		            });
+			});
+      	});
+  	});
+
+	//Testing the POST route without all parameters
+	describe('/POST /api/users', () => {
+      	it('it should not POST a user without password fields', (done) => {
+        	let group = {
+        		name: "Basic",
+        		p_cont_m:true,
+			    o_cont_m:false,
+			    manage_users:false,
+			    manage_groups:false,
+			    p_recp_m:true,
+			    o_recp_m:false,
+			    account_delete:true,
+			    deletable: false
+        	};
+        	models.collections.group.create(group, function(err, model) {
+			    if(err) return(err);
+
+			    let user = {
+	            	group: model.id,
+	            	name: "plop",
+	            	password: "plop"
+	        	};
+
+			   	chai.request("http://127.0.0.1:9002")
+	            	.post('/api/users')
+	            	.set('content-type', 'application/x-www-form-urlencoded')
+	            	.send(user)
+					.end(function(error, response) {
+		                let result=response.body.data;
+		               	response.should.have.status(200);
+		               	result.should.be.a('object');
+		               	result.should.have.property('name').eql("plop");
+		              	done();
+		            });
 			});
       	});
   	});
